f7106e4b9793f4600050802274388e81df1e5815
uscgithubuser <144652288+uscgithubuser@users.noreply.github.com>
2024-06-13 16:31:05 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "StackArray.h">
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	StackArrayList S;
+	initStack(&S);
+	int peek = Stack_Peek(S);
+	
+	
+	
+	
+
+} 
